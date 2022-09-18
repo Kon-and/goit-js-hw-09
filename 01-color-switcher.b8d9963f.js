779e418af6f933bfbef7bd8168a3c6d5b1a3bf58
Kon-and <97234816+Kon-and@users.noreply.github.com>
2022-09-18 22:04:24 +0000

    Deploying to gh-pages from @ Kon-and/goit-js-hw-09@36dc8d3d2cb56a71f26e0b088a7485048405b8bb 🚀

--- 01-color-switcher.b8d9963f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=0;function a(){document.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}btnStart.addEventListener("click",(function(){n=setInterval(a,1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.b8d9963f.js.map
